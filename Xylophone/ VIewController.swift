7e021d4eadc064db28069436475d3768a995738d
Valdivia, Jaime H <jaime.valdivia@astrazeneca.com>
2018-08-15 12:01:05 -0500

    Finished product

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate{
     
+    var audioPlayer: AVAudioPlayer!
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+        playSound(soundFileName: soundArray[sender.tag - 1])
     }
     
-  
-
+    func playSound(soundFileName: String) {
+        let soundUrl = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundUrl!)
+        } catch {
+            print(error)
+        }
+        
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+    }
+    
 }
-
